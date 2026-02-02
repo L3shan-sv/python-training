b408c1ae6c73d1adb05dcc9d5a77397cb796880f
mac <mac@l3shans-MacBook-Pro.local>
2026-02-02 14:00:34 +0300

    final script

--- tests.py
+++ tests.py
@@ -1,18 +1,34 @@
-pods=["running", "running", "pending", "crashloopback", "MMrom"]
-unhealty_count=0
+pods = ["running", "running", "pending", "running", "crashed", "running"]
+healthy_pods=0
+unhealthy_pods=0
+rollback_status=False
+tries=0
+max_tries=2
+
 
 for pod in pods:
-    if pod=="running":
-        print("pod healthy")
-        
-    else:
-        print("pod unhealthy")    
-        unhealty_count=(unhealty_count +1)\
-        
-    if unhealty_count==3:
-        print("system critical Please review")
+    if pod =="running":
+        print("Pod Healty, Continuing Rollout")
+        healthy_pods= healthy_pods+1
+        continue
+    while pod =="pending"and tries<max_tries:
+        print("Pod pending, Retrying")
+        tries= tries+1
+        if tries==max_tries:
+            print("Pod stuck, Stopping rollout")
+            unhealthy_pods=unhealthy_pods+1
+            rollback_status==True
+            break
+    if pod =="crashed":
+        print("Pod crashed, Rollback Initiated")
         break
-if unhealty_count<3:
-    print("system perfectly fine Ready for deployment")     
-    
+
+if unhealthy_pods<4:
+    print("Rollout Unstabale") 
+
+if rollback_status:
+    print("Rollout succesfull")
+else:
+    print("Rollout Complete")           
+
 
