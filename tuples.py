0eedda4134223bcdd7fb484331f30da35ba8b407
mac <mac@l3shans-MacBook-Pro.local>
2026-02-18 09:55:53 +0300

    python tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+students= ("Leshan", "20", "Cloud Engineer")
+
+
+print( students.index("Leshan"))
+
+if "Leshan" in students:
+    print("present")
+    
+for x in students: 
+    print( ''.join(reversed(x)))
+    
+    
