c90214f92ff6f4d95eb19409cc2e9ae993c06454
mac <mac@l3shans-MacBook-Pro.local>
2026-02-18 09:56:33 +0300

    python functions.py

---
+++ functions.py
@@ -0,0 +1,18 @@
+def check_cpu_usage(cpu_percentage):
+    for cpu_percentage in check_cpu_usage:
+        if cpu_percentage < 50:
+            return "Healty"
+        elif cpu_percentage <= cpu_percentage => 80:
+            return "Warning"
+        else:
+            return "critical"
+
+
+
+
+
+
+    
+
+        
+        
